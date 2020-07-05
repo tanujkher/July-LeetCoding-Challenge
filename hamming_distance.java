a39586a238bca900a47d2a91c5f81f2af1ff0178
tanujkher <tanujkher15@gmail.com>
2020-07-05 18:17:56 +0530

    Simple bit manipulation

---
+++ hamming_distance.java
@@ -0,0 +1,43 @@
+
+public class hamming_distance {
+    public static void main(String[] args) {
+        System.out.println("Simple bit manipulation");
+    }
+}
+
+class Solution {
+    public int hammingDistance(int x, int y) {
+        String s1 = toBinary(x);
+        String s2 = toBinary(y);
+        int i = 0;
+        int j = 0;
+        int ans = 0;
+        if(s1.length() < s2.length()){
+            while(s1.length() != s2.length()){
+                s1 = '0' + s1;
+            }
+        }
+        if(s1.length() > s2.length()){
+            while(s1.length() != s2.length()){
+                s2 = '0' + s2;
+            }
+        }
+        while(i <= s1.length() - 1 && j <= s2.length() - 1){
+            if(s1.charAt(i) != s2.charAt(j)){
+                ans++;
+            }
+            i++;
+            j++;
+        }
+        return ans;
+    }
+    private String toBinary(int x){
+        String ans = "";
+        while(x != 0){
+            int dig = x % 2;
+            x = x / 2;
+            ans = dig + ans;
+        }
+        return ans;
+    }
+}
